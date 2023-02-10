0ebc6ae4217bc81d2e92108f3a5d0bb1cd6513b3
dumbsito <lean.04river@gmail.com>
2023-02-10 15:37:46 -0300

    bits

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 var cors = require('cors')
 const app = express();
 const {Octokit}=require("@octokit/rest");
-const {createOAuthAppAuth, createOAuthUserAuth,} = require("@octokit/auth-oauth-app");
+const { asyncHandler } = require("./middleware");
+const {
+  createOAuthAppAuth,
+  createOAuthUserAuth,
+} = require("@octokit/auth-oauth-app");
+const { Router } = require("express");
+const { encryptToken, decryptToken } = require("./crypto");
+const secret_key="LuBEwq0eXNaPT8kpoQFZYHObmnKGsM93c5IJx2SgCRUlv4Ayd6Vh1zWiD7rtjf"
+const router=Router();
 const clientId = 'a727daaaf35737b322cb';
 const clientSecret = '37f2a8358ed451f294626c6835982ce35a39dfce';
 
@@ -14,18 +22,21 @@ app.use(function(req, res, next) {
 });
 
 
-
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 
 app.get('/lol', (req, res) => {
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}`);
+
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}`);
+
+
 });
 
 app.listen(3000);
 console.log('App listening on port 3000');
 
 const axios = require('axios');
-var access_token = "";
+let access_token = "";
 
 app.get('/oauth-callback', (req, res) => {
   const body = {
@@ -35,14 +46,32 @@ app.get('/oauth-callback', (req, res) => {
   };
   const opts = { headers: { accept: 'application/json' } };
 
+ 
+ 
   axios.post(`https://github.com/login/oauth/access_token`, body, opts).
   then((response) => {
-    access_token = response.data.access_token
+    
+    access_token = response.data.access_token;
     res.redirect('http://localhost:4200/dashboard');
  // res.send("<h1>Bien hecho!!</h1>")
   }).
     catch(err => res.status(500).json({ message: err.message }));
-    console.log(body.code)
+
+   /* const appAuth = createOAuthAppAuth({
+      clientType: "oauth-app",
+      clientId:clientId,
+      clientSecret: clientSecret,
+    });
+  
+    const userAuth = await appAuth({
+      type: "oauth-user",
+      code: body.code,
+      factory: createOAuthUserAuth,
+    });
+    const authentication = await userAuth();
+    const { token } = authentication;*/
+    
+
 });
 
 
@@ -115,21 +144,101 @@ app.get('/oauth-callback', (req, res) => {
       }
     }).then((response) => {
      
-      res.json({ userData: response.data });
+     
     
     })
   });
 
-app.get("/logout", (req,res)=>{
+  app.get("/cb", asyncHandler(async (req, res, next) => {
+
+      res.redirect(`https://localhost:4200`);
+    })
+  );
+
+
+
+  app.get("/logout", async (req, res) => {
+    /* get the encrypted toke from the req cookies
+      const encryptedToken = req.cookies.session_id;
+      
+      const token = decryptToken(encryptedToken, secret_key);*/
+     
+      // app authentication
+      const appOctokit = new Octokit({
+        authStrategy: createOAuthAppAuth,
+        auth: {
+          clientId: clientId,
+          clientSecret: clientSecret,
+        },
+      });
   
- 
+      // revoke user's access token
+      await appOctokit.request("DELETE /applications/{client_id}/grant", {
+        client_id: clientId,
+        access_token: access_token,
+      });
+    
+      // redirect user to home page
+      res.redirect(`https://localhost:4200`);
+    })
   
   
-res.redirect("http://localhost:4200")
-})
+   
 
 
   /*
+   const auth = createTokenAuth(access_token);
+    const authentication = await auth();
+
+
+     res.send(authentication)
+  
+  axios({
+      method: 'get',
+      url: `https://api.github.com/applications/a727daaaf35737b322cb/token`,
+      headers: {
+        Authorization: 'token ' + access_token
+      }
+    }).then((response) => {
+     
+      res.redirect("http://localhost:4200")
+    })
+     const octokit = new Octokit({
+      
+      auth: access_token })
+
+     await octokit.request('DELETE /applications/{client_id}/token', {
+        
+        client_id: 'a727daaaf35737b322cb',
+        access_token: access_token 
+     })
+     
+      const result=   await octokit.request("GET /repos/{owner}/{repo}/issues", {
+      owner: "dumbsito",
+      repo: "notes-app",
+    });
+    try{
+     const octokit = new Octokit({
+      auth: access_token
+    });
+
+ 
+  await octokit.request('DELETE /applications/{client_id}/token', {
+      client_id: 'a727daaaf35737b322cb'
+   })
+  }catch(err){
+    console.log("el error es", err);
+  }*/
+
+    
+ 
+
+
+
+
+  /*
+  const octokit = new Octokit({ auth: 'YOUR-TOKEN' }) await octokit.request('DELETE /applications/{client_id}/grant', { client_id: 'Iv1.8a61f9b3a7aba766', access_token: 'e72e16c7e42f292c6912e7710c838347ae178b4a' })
+  const octokit = new Octokit({ auth: 'YOUR-TOKEN' }) await octokit.request('DELETE /applications/{client_id}/token', { client_id: 'Iv1.8a61f9b3a7aba766', access_token: 'e72e16c7e42f292c6912e7710c838347ae178b4a' })
   app.get('/success',(req, res) => {
    const headers= {
       Authorization: 'token ' + access_token
@@ -144,7 +253,7 @@ res.redirect("http://localhost:4200")
   
    axios({
     method: 'delete',
-    url: `http://localhost:3000/logout`,
+    url: `https://github.com/settings/connections/applications/a727daaaf35737b322cb`,
     headers: {
       Authorization: 'token ' + access_token
     },
@@ -159,3 +268,34 @@ res.redirect("http://localhost:4200")
   
   */
 
+
+
+
+/*  router.get("/logout",
+
+
+    asyncHandler(async (req, res, next) => {
+      // get the encrypted toke from the req cookies
+      const encryptedToken = req.cookies.session_id;
+      const token = decryptToken(encryptedToken, process.env.SECRETE_KEY);
+  
+      // app authentication
+      const appOctokit = new Octokit({
+        authStrategy: createOAuthAppAuth,
+        auth: {
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRETE,
+        },
+      });
+  
+      // revoke user's access token
+      await appOctokit.request("DELETE /applications/{client_id}/grant", {
+        client_id: clientId,
+        access_token: token,
+      });
+    
+      // redirect user to home page
+      res.redirect(`${process.env.CLIENT_URL}`);
+    })
+   
+)*/
